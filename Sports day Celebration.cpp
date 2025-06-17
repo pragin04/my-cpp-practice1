c552ef2280a948a62664f8d902a77465e9ba5e87
pragin04 <pragin29@gmail.com>
2025-06-17 15:34:26 +0530

    Create Sports day Celebration.cpp

---
+++ Sports day Celebration.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=60,b=8;
+    cout<<a%b<<endl;
+    cout<<a/b<<endl;
+    return 0;
+    
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=50,b=4;
+    cout<<a%b<<endl;
+    cout<<a/b<<endl;
+    return 0;
+    
+}
