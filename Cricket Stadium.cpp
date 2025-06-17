75cc15ec9648d2c668890c924ea1521f813ced1c
pragin04 <pragin29@gmail.com>
2025-06-17 15:28:37 +0530

    Create  Cricket Stadium.cpp

---
+++ Cricket Stadium.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=50,b=20;
+    cout<<a*b<<endl;
+    cout<<(a*2)+(b*2)<<endl;
+    return 0;
+    
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=70,b=40;
+    cout<<a*b<<endl;
+    cout<<(a*2)+(b*2)<<endl;
+    return 0;
+    
+}
