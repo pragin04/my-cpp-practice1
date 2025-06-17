3b6f316093cf6e7304e628c840f721e88c67b2f5
pragin04 <pragin29@gmail.com>
2025-06-17 16:00:24 +0530

    Create The newspaper Agency.cpp

---
+++ The newspaper Agency.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input1=1000, input2=2, input3=1;
+    
+    cout<<input1 - (input1 * input2 * input3) / 20<<endl;
+
+    return 0;
+}
